fa79454f5abc29a49a29bbec9f77c3c634c8d9b2
mperrinel <mperrinel@itlink.fr>
2017-06-02 11:04:57 +0200

    Remove Qt_PATH variable from de clang analyzer launcher file since we assume using the QT native path.

--- analyzer/launch-clang-analyzer-linux.sh
+++ analyzer/launch-clang-analyzer-linux.sh
@@ -1,6 +1,4 @@
 
-QT_PATH=".../Qt/5.8/gcc_64/lib/cmake/"
-
 export CC=/usr/libexec/ccc-analyzer
 export CXX=/usr/libexec/c++-analyzer
 export CCC_CC=clang
@@ -15,6 +13,6 @@ rm -rf build_clang-analyzer
 mkdir build_clang-analyzer
 cd build_clang-analyzer
 
-scan-build cmake -DCMAKE_PREFIX_PATH=$QT_PATH -DCMAKE_CXX_COMPILER=clazy -DENABLE_ANALYSIS=false -DENABLE_CPPCHECK=false -DENABLE_FORMATTING=false -DENABLE_CHECKSTYLE=false -BUILD_DOCUMENTATION=false -BUILD_TESTS=false -DCMAKE_BUILD_TYPE=Debug ../../SCIQLOP-Initialisation/
+scan-build cmake -DCMAKE_CXX_COMPILER=clazy -DENABLE_ANALYSIS=false -DENABLE_CPPCHECK=false -DENABLE_FORMATTING=false -DENABLE_CHECKSTYLE=false -BUILD_DOCUMENTATION=false -BUILD_TESTS=false -DCMAKE_BUILD_TYPE=Debug ../../SCIQLOP-Initialisation/
 
 scan-build -o clang-analyzer-output make -j2
