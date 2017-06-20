e7b5abd4403a5ce2bdffcadc8643eade305ba146
mperrinel <mperrinel@itlink.fr>
2017-06-20 14:12:58 +0200

    Add SqpDateTime struct

--- core/include/Data/DataProviderParameters.h
+++ core/include/Data/DataProviderParameters.h
@@ -1,16 +1,15 @@
 #ifndef SCIQLOP_DATAPROVIDERPARAMETERS_H
 #define SCIQLOP_DATAPROVIDERPARAMETERS_H
 
+#include "SqpDateTime.h"
+
 /**
  * @brief The DataProviderParameters struct holds the information needed to retrieve data from a
  * data provider
  * @sa IDataProvider
  */
 struct DataProviderParameters {
-    /// Start time
-    double m_TStart;
-    /// End time
-    double m_TEnd;
+    SqpDateTime m_Time;
 };
 
 #endif // SCIQLOP_DATAPROVIDERPARAMETERS_H
---
+++ core/include/Data/SqpDateTime.h
@@ -0,0 +1,15 @@
+#ifndef SCIQLOP_SQPDATETIME_H
+#define SCIQLOP_SQPDATETIME_H
+
+/**
+ * @brief The SqpDateTime struct holds the information of time parameters
+ * @sa SqpDateTime
+ */
+struct SqpDateTime {
+    /// Start time
+    double m_TStart;
+    /// End time
+    double m_TEnd;
+};
+
+#endif // SCIQLOP_SQPDATETIME_H
