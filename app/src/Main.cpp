55810e577c681af2a8d6818c5d7f5e53ab94b14d
Alexandre Leroux <aleroux@itlink.fr>
2017-06-29 10:08:39 +0200

    Fixes compilation

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 #endif
 
 
-#if __GNUC__
+#if __LINUX__ || __APPLE__
 #if __x86_64__ || __ppc64__
     if (!pluginDir.cd("../lib64/SciQlop")) {
         pluginDir.cd("../lib64/sciqlop");
