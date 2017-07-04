b2a47c61d04d12fbe542f025bbe1ed312902fb60
Alexandre Leroux <aleroux@itlink.fr>
2017-07-04 12:36:11 +0200

    Initializes plugin

---
+++ plugins/amda/src/AmdaPlugin.cpp
@@ -0,0 +1,29 @@
+#include "AmdaPlugin.h"
+
+#include <DataSource/DataSourceController.h>
+
+#include <SqpApplication.h>
+
+Q_LOGGING_CATEGORY(LOG_AmdaPlugin, "AmdaPlugin")
+
+namespace {
+
+/// Name of the data source
+const auto DATA_SOURCE_NAME = QStringLiteral("AMDA");
+
+} // namespace
+
+void AmdaPlugin::initialize()
+{
+    if (auto app = sqpApp) {
+        // Registers to the data source controller
+        auto &dataSourceController = app->dataSourceController();
+        auto dataSourceUid = dataSourceController.registerDataSource(DATA_SOURCE_NAME);
+
+        // Sets data source tree
+        /// @todo ALX
+    }
+    else {
+        qCWarning(LOG_AmdaPlugin()) << tr("Can't access to SciQlop application");
+    }
+}
