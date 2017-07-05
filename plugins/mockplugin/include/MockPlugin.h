057ef7ee6e0db9c9d9028b75195015e554d1d99f
Alexandre Leroux <aleroux@itlink.fr>
2017-07-05 11:26:11 +0200

    Adds Q_DECL_EXPORT for Mock plugin

--- plugins/mockplugin/include/MockPlugin.h
+++ plugins/mockplugin/include/MockPlugin.h
@@ -1,6 +1,8 @@
 #ifndef SCIQLOP_MOCKPLUGIN_H
 #define SCIQLOP_MOCKPLUGIN_H
 
+#include "MockPluginGlobal.h"
+
 #include <Plugin/IPlugin.h>
 
 #include <QLoggingCategory>
@@ -11,7 +13,7 @@ Q_DECLARE_LOGGING_CATEGORY(LOG_MockPlugin)
 
 class DataSourceItem;
 
-class MockPlugin : public QObject, public IPlugin {
+class SCIQLOP_MOCKPLUGIN_EXPORT MockPlugin : public QObject, public IPlugin {
     Q_OBJECT
     Q_INTERFACES(IPlugin)
     Q_PLUGIN_METADATA(IID "sciqlop.plugin.IPlugin" FILE "mockplugin.json")
