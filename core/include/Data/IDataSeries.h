4c5c8ac48250ebe771c00f3ac52ff1e1906f3250
Alexandre Leroux <aleroux@itlink.fr>
2017-07-10 15:49:43 +0200

    Units tests structure

--- core/include/Data/IDataSeries.h
+++ core/include/Data/IDataSeries.h
@@ -16,6 +16,12 @@ struct Unit {
     {
     }
 
+    inline bool operator==(const Unit &other) const
+    {
+        return std::tie(m_Name, m_TimeUnit) == std::tie(other.m_Name, other.m_TimeUnit);
+    }
+    inline bool operator!=(const Unit &other) const { return !(*this == other); }
+
     QString m_Name;  ///< Unit name
     bool m_TimeUnit; ///< The unit is a unit of time
 };
--- plugins/amda/tests/TestAmdaResultParser.cpp
+++ plugins/amda/tests/TestAmdaResultParser.cpp
@@ -14,7 +14,61 @@ QString inputFilePath(const QString &inputFileName)
     return QFileInfo{TESTS_RESOURCES_PATH, inputFileName}.absoluteFilePath();
 }
 
+struct ExpectedResults {
+    explicit ExpectedResults() = default;
+
+    /// Ctor with QVector<QDateTime> as x-axis data. Datetimes are converted to doubles
+    explicit ExpectedResults(Unit xAxisUnit, Unit valuesUnit, const QVector<QDateTime> &xAxisData,
+                             QVector<double> valuesData)
+            : m_ParsingOK{true},
+              m_XAxisUnit{xAxisUnit},
+              m_ValuesUnit{valuesUnit},
+              m_XAxisData{},
+              m_ValuesData{std::move(valuesData)}
+    {
+        // Converts QVector<QDateTime> to QVector<double>
+        std::transform(xAxisData.cbegin(), xAxisData.cend(), std::back_inserter(m_XAxisData),
+                       [](const auto &dateTime) { return dateTime.toMSecsSinceEpoch() / 1000.; });
+    }
+
+    /**
+     * Validates a DataSeries compared to the expected results
+     * @param results the DataSeries to validate
+     */
+    void validate(std::shared_ptr<IDataSeries> results)
+    {
+        if (m_ParsingOK) {
+            auto scalarSeries = dynamic_cast<ScalarSeries *>(results.get());
+            QVERIFY(scalarSeries != nullptr);
+
+            // Checks units
+            QVERIFY(scalarSeries->xAxisUnit() == m_XAxisUnit);
+            QVERIFY(scalarSeries->valuesUnit() == m_ValuesUnit);
+
+            // Checks values
+            QVERIFY(scalarSeries->xAxisData()->data() == m_XAxisData);
+            QVERIFY(scalarSeries->valuesData()->data() == m_ValuesData);
+        }
+        else {
+            QVERIFY(results == nullptr);
+        }
+    }
+
+    // Parsing was successfully completed
+    bool m_ParsingOK{false};
+    // Expected x-axis unit
+    Unit m_XAxisUnit{};
+    // Expected values unit
+    Unit m_ValuesUnit{};
+    // Expected x-axis data
+    QVector<double> m_XAxisData{};
+    // Expected values data
+    QVector<double> m_ValuesData{};
+};
+
 } // namespace
+
+Q_DECLARE_METATYPE(ExpectedResults)
 
 class TestAmdaResultParser : public QObject {
     Q_OBJECT
@@ -29,10 +83,30 @@ private slots:
 
 void TestAmdaResultParser::testReadTxt_data()
 {
+    // ////////////// //
+    // Test structure //
+    // ////////////// //
+
+    // Name of TXT file to read
+    QTest::addColumn<QString>("inputFileName");
+    // Expected results
+    QTest::addColumn<ExpectedResults>("expectedResults");
+
 }
 
 void TestAmdaResultParser::testReadTxt()
 {
+    QFETCH(QString, inputFileName);
+    QFETCH(ExpectedResults, expectedResults);
+
+    // Parses file
+    auto filePath = inputFilePath(inputFileName);
+    auto results = AmdaResultParser::readTxt(filePath);
+
+    // ///////////////// //
+    // Validates results //
+    // ///////////////// //
+    expectedResults.validate(results);
 }
 
 QTEST_MAIN(TestAmdaResultParser)
