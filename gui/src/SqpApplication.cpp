d987dda795cf490d7f472f2d12d7d497a840595c
mperrinel <mperrinel@itlink.fr>
2017-07-17 18:03:29 +0200

    Remove connection for progress from NC -> VC to NC -> Provider.

--- gui/src/SqpApplication.cpp
+++ gui/src/SqpApplication.cpp
@@ -106,11 +106,6 @@ SqpApplication::SqpApplication(int &argc, char **argv)
     impl->m_NetworkControllerThread.start();
     impl->m_VariableControllerThread.start();
     impl->m_VisualizationControllerThread.start();
-
-    // Core connections:
-    // NetworkController <-> VariableController
-    connect(&sqpApp->networkController(), &NetworkController::replyDownloadProgress,
-            &sqpApp->variableController(), &VariableController::onVariableRetrieveDataInProgress);
 }
 
 SqpApplication::~SqpApplication()
