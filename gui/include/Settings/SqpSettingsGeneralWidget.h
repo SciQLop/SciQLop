bf267432931ba87af325fa11207086b72bfd300c
Alexandre Leroux <aleroux@itlink.fr>
2017-07-27 12:38:00 +0200

    Settings binding (2)
    Makes widget for general settings a ISqpSettingsBindable and handles load and save

--- gui/include/Settings/SqpSettingsGeneralWidget.h
+++ gui/include/Settings/SqpSettingsGeneralWidget.h
@@ -1,6 +1,8 @@
 #ifndef SCIQLOP_SQPSETTINGSGENERALWIDGET_H
 #define SCIQLOP_SQPSETTINGSGENERALWIDGET_H
 
+#include "Settings/ISqpSettingsBindable.h"
+
 #include <QWidget>
 
 namespace Ui {
@@ -10,12 +12,18 @@ class SqpSettingsGeneralWidget;
 /**
  * @brief The SqpSettingsGeneralWidget class represents the general settings of SciQlop
  */
-class SqpSettingsGeneralWidget : public QWidget {
+class SqpSettingsGeneralWidget : public QWidget, public ISqpSettingsBindable {
     Q_OBJECT
 
 public:
     explicit SqpSettingsGeneralWidget(QWidget *parent = 0);
     virtual ~SqpSettingsGeneralWidget() noexcept;
+
+    /// @sa ISqpSettingsBindable::loadSettings()
+    void loadSettings() override final;
+
+    /// @sa ISqpSettingsBindable::saveSettings()
+    void saveSettings() const override final;
 
 private:
     Ui::SqpSettingsGeneralWidget *ui;
