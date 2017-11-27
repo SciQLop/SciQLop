222545cbc90a8b8a06a5c484f33019b7127f0f5f
Alexandre Leroux <aleroux@itlink.fr>
2017-11-27 17:48:49 +0100

    Creates AMDA implementations (default and AMDA test)

--- plugins/amda/src/AmdaServer.cpp
+++ plugins/amda/src/AmdaServer.cpp
@@ -2,6 +2,24 @@
 
 Q_LOGGING_CATEGORY(LOG_AmdaServer, "AmdaServer")
 
+namespace {
+
+/// Default AMDA server
+struct AmdaDefaultServer : public AmdaServer {
+public:
+    QString name() const override { return QStringLiteral("AMDA (default)"); }
+    QString url() const override { return QStringLiteral("amda.irap.omp.eu"); }
+};
+
+/// Alternative AMDA server (tests)
+struct AmdaTestServer : public AmdaServer {
+public:
+    QString name() const override { return QStringLiteral("AMDA (test)"); }
+    QString url() const override { return QStringLiteral("amdatest.irap.omp.eu"); }
+};
+
+} // namespace
+
 AmdaServer &AmdaServer::instance()
 {
     /// @todo ALX
