94f6e8c9cecc9f99383cbdd6eb66401787786be4
Alexandre Leroux <aleroux@itlink.fr>
2017-12-08 14:46:04 +0100

    Implements "Variable creation" operation (2)

--- plugins/amda/tests/FuzzingOperations.cpp
+++ plugins/amda/tests/FuzzingOperations.cpp
@@ -1,7 +1,12 @@
 #include "FuzzingOperations.h"
+#include "FuzzingUtils.h"
+
+#include <Data/IDataProvider.h>
 
 #include <Variable/Variable.h>
 #include <Variable/VariableController.h>
+
+#include <QUuid>
 
 Q_LOGGING_CATEGORY(LOG_FuzzingOperations, "FuzzingOperations")
 
@@ -9,12 +14,23 @@ namespace {
 
 struct CreateOperation : public IFuzzingOperation {
     bool canExecute(std::shared_ptr<Variable> variable) const override {
-        /// @todo: complete
-        return false;
+        // A variable can be created only if it doesn't exist yet
+        return variable == nullptr;
     }
 
     void execute(std::shared_ptr<Variable>& variable, VariableController &variableController, const Properties &properties) const override{
-        /// @todo: complete
+        // Retrieves metadata pool from properties, and choose one of the metadata entries to associate it with the variable
+        auto metaDataPool = properties.value(METADATA_POOL_PROPERTY).value<MetadataPool>();
+        auto variableMetadata = RandomGenerator::instance().randomChoice(metaDataPool);
+
+        // Retrieves provider
+        auto variableProvider = properties.value(PROVIDER_PROPERTY).value<std::shared_ptr<IDataProvider>>();
+
+        auto variableName = QString{"Var_%1"}.arg(QUuid::createUuid().toString());
+        qCInfo(LOG_FuzzingOperations()) << "Creating variable" << variableName << "(metadata:" << variableMetadata << ")";
+
+        auto newVariable = variableController.createVariable(variableName, variableMetadata, variableProvider);
+        std::swap(variable, newVariable);
     }
 };
 
