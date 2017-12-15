23206e07bbc2fd7f017c330e8c8c8ef0a9bd8a10
Alexandre Leroux <aleroux@itlink.fr>
2017-12-15 15:42:09 +0100

    Reads variable's metadata to retrieve the type of data series (scalar, vector, spectrogram)

--- core/include/Variable/Variable.h
+++ core/include/Variable/Variable.h
@@ -4,6 +4,7 @@
 #include "CoreGlobal.h"
 
 #include <Data/DataSeriesIterator.h>
+#include <Data/DataSeriesType.h>
 #include <Data/SqpRange.h>
 
 #include <QLoggingCategory>
@@ -53,6 +54,9 @@ public:
 
     /// @return the data of the variable, nullptr if there is no data
     std::shared_ptr<IDataSeries> dataSeries() const noexcept;
+
+    /// @return the type of data that the variable holds
+    DataSeriesType type() const noexcept;
 
     QVariantHash metadata() const noexcept;
 
