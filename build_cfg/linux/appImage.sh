66d22dea2383a0eb343e8e1a7e537a8af7466172
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-01-17 02:13:03 +0100

    Switched to linuxdeployqt tool for AppImage build (tested on centos 7 with devtoolset-7 and Qt 5.11)
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_cfg/linux/appImage.sh
+++ build_cfg/linux/appImage.sh
@@ -4,10 +4,6 @@ cd build
 meson --prefix=/usr ..
 ninja
 DESTDIR=AppDir ninja install
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-chmod +x linuxdeploy-x86_64.AppImage
-./linuxdeploy-x86_64.AppImage --appimage-extract
-LD_LIBRARY_PATH=AppDir/usr/lib/ ./squashfs-root/AppRun --appdir AppDir
-mv ./AppDir/usr/lib64/*.so ./AppDir/usr/lib/
-mv ./AppDir/usr/lib/*plugin.so ./AppDir/usr/bin/
-LD_LIBRARY_PATH=AppDir/usr/lib/ ./squashfs-root/AppRun --appdir AppDir --output appimage
+wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+chmod +x  linuxdeployqt-continuous-x86_64.AppImage && ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
+LD_LIBRARY_PATH=AppDir/usr/lib64/ ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage
