410bc20846a0bad5eb82730e0012f5176df038de
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-01-18 00:28:01 +0100

    Tinny AppImage fix
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_cfg/linux/appImage.sh
+++ build_cfg/linux/appImage.sh
@@ -4,6 +4,8 @@ cd build
 meson --prefix=/usr ..
 ninja
 DESTDIR=AppDir ninja install
+mv AppDir/usr/lib64 AppDir/usr/lib
+mv AppDir/usr/lib/*plugin.so AppDir/usr/bin/
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 chmod +x  linuxdeployqt-continuous-x86_64.AppImage && ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
 LD_LIBRARY_PATH=AppDir/usr/lib64/ ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage
