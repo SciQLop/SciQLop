655463cb68b278f8f878f60e0b2842dee3f654ce
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-01-18 03:00:44 +0100

    Fixed OSX build

--- build_cfg/mac/build.sh
+++ build_cfg/mac/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir build
-meson --prefix=/tmp/SciQLOP.app --bindir=Contents/MacOS build
+meson -Dcpp_args='-DQT_STATICPLUGIN' -Ddefault_library=static --prefix=/tmp/SciQLOP.app --bindir=Contents/MacOS build
 cd build
 ninja
 ninja install
