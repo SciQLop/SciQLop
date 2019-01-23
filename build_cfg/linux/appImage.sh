2aae0bf5374da7b150b71e03f4991449f0e6d131
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-01-24 00:08:22 +0100

    Some AppImage changes
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_cfg/linux/appImage.sh
+++ build_cfg/linux/appImage.sh
@@ -7,4 +7,4 @@ DESTDIR=AppDir ninja install
 mv AppDir/usr/lib64 AppDir/usr/lib
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 chmod +x  linuxdeployqt-continuous-x86_64.AppImage && ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
-LD_LIBRARY_PATH=AppDir/usr/lib/ ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage
+LD_LIBRARY_PATH=AppDir/usr/lib/ ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines,platformthemes/libqgtk3.so
