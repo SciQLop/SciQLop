210a11bf5a65d8e5ee1f1f26abcfa07c7838b93c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-04-12 17:57:21 +0200

    Added ability to drop data columns with CDAWEB plugin
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- plugins/python_providers/resources/cdaweb.py
+++ plugins/python_providers/resources/cdaweb.py
@@ -17,11 +17,14 @@ def cda_get_sample(metadata, start,stop):
     try:
         variable_id = None
         dataset_id = None
+        drop_cols = []
         for key,value in metadata:
             if key == 'VAR_ID':
                 variable_id = value
             elif key == 'DATASET_ID':
                 dataset_id = value
+            elif key == 'drop_col':
+                drop_cols.append(value)
             elif key == 'type':
                 if value == 'vector':
                     ts_type = pysciqlopcore.VectorTimeSerie
@@ -31,19 +34,20 @@ def cda_get_sample(metadata, start,stop):
         tstart=datetime.datetime.fromtimestamp(start, tz=timezone.utc)
         tend=datetime.datetime.fromtimestamp(stop, tz=timezone.utc)
         df = cd.get_variable(dataset=dataset_id,variable=variable_id,tstart=tstart,tend=tend)
+        if len(df):
+            df = df.drop(columns = drop_cols)
         t = np.array([d.timestamp() for d in df.index])
         values = df.values
-        print(values.shape)
         return ts_type(t,values)
     except Exception as e: 
         print(traceback.format_exc())
-        print("Error in amda.py ",str(e))
+        print("Error in cdaweb.py ",str(e))
         return ts_type(default_ctor_args)
 
 
 products = [
-   ("/CDA/Themis/ThA/tha_fgl_gsm", [], [("type","multicomponent"), ('size','4'), ("DATASET_ID","THA_L2_FGM"), ("VAR_ID","tha_fgl_gsm")]),
-   ("/CDA/Themis/ThB/thb_fgl_gsm", [], [("type","multicomponent"), ('size','4'), ("DATASET_ID","THB_L2_FGM"), ("VAR_ID","thb_fgl_gsm")]),
+   ("/CDA/Themis/ThA/tha_fgl_gsm", [], [("type","vector"), ('drop_col','UT__sec'), ("DATASET_ID","THA_L2_FGM"), ("VAR_ID","tha_fgl_gsm")]),
+   ("/CDA/Themis/ThB/thb_fgl_gsm", [], [("type","vector"), ('drop_col','UT__sec'), ("DATASET_ID","THB_L2_FGM"), ("VAR_ID","thb_fgl_gsm")]),
  
 ]
 
