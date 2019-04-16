7b301118a3a58fc0a5cc63c27fb2051f0c4c5a9a
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-04-16 19:46:21 +0200

    AppImage Python test
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_cfg/linux/appImage.sh
+++ build_cfg/linux/appImage.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+HERE="$(dirname "$(readlink -f "${0}")")"
 mkdir build
 cd build
 mkdir -p $(pwd)/AppDir/usr
+cp $HERE/AppRun $(pwd)/AppDir/
+chmod +x $(pwd)/AppDir/AppRun
 virtualenv -p python3 $(pwd)/AppDir/usr
-source $(pwd)/AppDir/usr/bin/activate
-pip install git+https://github.com/jeandet/spwc
+$(pwd)/AppDir/usr/bin/pip3 install git+https://github.com/jeandet/spwc
 meson --prefix=/usr ..
 ninja
 DESTDIR=$(pwd)/AppDir ninja install
 mv AppDir/usr/lib64 AppDir/usr/lib
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 chmod +x  linuxdeployqt-continuous-x86_64.AppImage && ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
-LD_LIBRARY_PATH=AppDir/usr/lib/:AppDir/usr/lib/SciQLop/ ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines,platformthemes/libqgtk3.so
+LD_LIBRARY_PATH=AppDir/usr/lib/:AppDir/usr/lib/SciQLop/:$(python3-config --prefix)/lib64 ./squashfs-root/AppRun AppDir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines,platformthemes/libqgtk3.so
