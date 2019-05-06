021e7c505ad430cbbb43dc20fd356eea9ed50869
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-05-06 10:09:20 +0200

    Small GUI fix, show full names in repository tree view
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -45,11 +45,8 @@ int main(int argc, char* argv[])
 {
 #ifdef QT_STATICPLUGIN
 #ifndef SQP_NO_PLUGINS
-    //Q_IMPORT_PLUGIN(MockPlugin)
     Q_IMPORT_PLUGIN(PythonProviders)
     Q_INIT_RESOURCE(python_providers);
-    // Q_IMPORT_PLUGIN(AmdaPlugin)
-    // Q_INIT_RESOURCE(amdaresources);
 #endif
 #endif
     Q_INIT_RESOURCE(sqpguiresources);
