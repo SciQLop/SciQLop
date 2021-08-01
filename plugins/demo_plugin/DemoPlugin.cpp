8ca0c68a5267ea41e27d216bf0b0ab754f30629c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-08-01 17:20:38 +0200

    Completed basic C++ plugin with a really basic data provider
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- plugins/demo_plugin/DemoPlugin.cpp
+++ plugins/demo_plugin/DemoPlugin.cpp
@@ -1,7 +1,13 @@
 #include "DemoPlugin.hpp"
 #include <iostream>
+#include <DataSource/datasources.h>
+#include <SqpApplication.h>
+
+#include "DataSource.hpp"
 
 void DemoPlugin::initialize()
 {
     std::cout << "Loading DemoPlugin \\o/" << std::endl;
+    auto& dataSources = sqpApp->dataSources();
+    dataSources.addProvider<DataSource>();
 }
