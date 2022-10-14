9199e1775377722cbc4da246be90a4716587f395
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-10-14 16:26:04 +0200

    few fixes for latest speasy release
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/plugins/speasy.py
+++ SciQLop/plugins/speasy.py
@@ -2,37 +2,57 @@ from SciQLopBindings import DataProvider, Product, ScalarTimeSerie, VectorTimeSe
 from SciQLopBindings import SciQLopCore, MainWindow, TimeSyncPanel, ProductsTree, DataSeriesType
 import numpy as np
 
+from typing import Dict
 import speasy as spz
-from speasy.inventory.indexes import ParameterIndex, ComponentIndex
-from speasy.inventory.data_tree import amda
+from speasy.products import SpeasyVariable
+from speasy.core.inventory.indexes import ParameterIndex, ComponentIndex, SpeasyIndex
+from datetime import datetime
 
 
 def explore_nodes(node, path, leaves):
     for name, child in node.__dict__.items():
-        cur_path = path + "/" + name
-        if isinstance(child, ParameterIndex):
-            leaves[cur_path] = child
-        elif hasattr(child, "__dict__"):
-            explore_nodes(child, cur_path, leaves)
+        if name and child:
+            cur_path = path + "/" + name
+            if isinstance(child, ParameterIndex):
+                leaves[cur_path] = child
+            elif hasattr(child, "__dict__"):
+                explore_nodes(child, cur_path, leaves)
 
 
 def count_components(param: ParameterIndex):
     if hasattr(param, "size"):
         return int(param.size)
+    if hasattr(param, 'LABL_PTR_1'):
+        return len(param.LABL_PTR_1.split(','))
+    if hasattr(param, 'LABLAXIS'):
+        return len(param.LABLAXIS.split(','))
+    if hasattr(param, 'array_dimension') and param.array_dimension != "":
+        return int(param.array_dimension.split(':')[-1])
+    if param.spz_provider() == 'ssc':
+        return 3
     return 0
 
 
 def data_serie_type(param: ParameterIndex):
     if hasattr(param, "display_type"):
-        if param.display_type == 'timeseries':
-            components_cnt = count_components(param)
-            if components_cnt == 0:
+        display_type = param.display_type
+    elif hasattr(param, "DISPLAY_TYPE"):
+        display_type = param.DISPLAY_TYPE
+    elif param.spz_provider() == 'ssc':
+        display_type = 'timeseries'
+    else:
+        display_type = None
+    components_cnt = count_components(param)
+    if display_type is not None or components_cnt != 0:
+        if display_type == 'spectrogram':
+            return DataSeriesType.SPECTROGRAM
+        else:
+            if components_cnt == 0 or components_cnt == 1:
                 return DataSeriesType.SCALAR
             if components_cnt == 3:
                 return DataSeriesType.VECTOR
             return DataSeriesType.MULTICOMPONENT
-        if param.display_type == 'spectrogram':
-            return DataSeriesType.SPECTROGRAM
+
     return DataSeriesType.NONE
 
 
@@ -53,41 +73,56 @@ type_str = {
 }
 
 
-def make_product(path, node):
+def make_product(path, node: ParameterIndex):
     p_type = data_serie_type(node)
     comp = count_components(node)
     meta = get_node_meta(node)
-    meta["xmlid"] = node.xmlid
+    meta["uid"] = node.spz_uid()
     meta["components"] = str(comp)
     meta["type"] = type_str[p_type]
+    meta["provider"] = node.spz_provider()
     return Product(path, [], p_type, meta)
 
 
 class SpeasyPlugin(DataProvider):
     def __init__(self, parent=None):
         super(SpeasyPlugin, self).__init__(parent)
-        leaves = {}
-        explore_nodes(amda, 'amda', leaves)
+        leaves: Dict[str, SpeasyIndex] = {}
+        print(f"start: {datetime.now()}")
+        explore_nodes(spz.inventories.tree, 'speasy', leaves)
         self.products = {
-            node.xmlid: make_product(path, node)
+            node.spz_uid(): make_product(path, node)
             for path, node in leaves.items()
         }
+        print(f"stop: {datetime.now()}")
         self.register_products(list(self.products.values()))
 
     def get_data(self, metadata, start, stop):
-        p = self.products[metadata["xmlid"]]
-        v = spz.get_data("amda/" + metadata["xmlid"], start, stop)
+        print(metadata)
+        p = self.products[metadata["uid"]]
+        print(p, metadata["uid"], metadata["provider"], datetime.utcfromtimestamp(start),
+              datetime.utcfromtimestamp(stop))
+        try:
+            v: SpeasyVariable = spz.get_data(metadata["provider"] + "/" + metadata["uid"],
+                                             datetime.utcfromtimestamp(start),
+                                             datetime.utcfromtimestamp(stop))
+            print(f"got data: {v}")
+            if v:
+                v.replace_fillval_by_nan(inplace=True)
+        except Exception as e:
+            print(e)
+            return None
         if p.ds_type == DataSeriesType.SCALAR:
             return ScalarTimeSerie(
-                v.time, v.data
+                v.time.astype(np.timedelta64) / np.timedelta64(1, 's'), v.values.astype(np.float)
             ) if v else ScalarTimeSerie(np.array([]), np.array([]))
         if p.ds_type == DataSeriesType.VECTOR:
             return VectorTimeSerie(
-                v.time, v.data
+                v.time.astype(np.timedelta64) / np.timedelta64(1, 's'), v.values.astype(np.float)
             ) if v else VectorTimeSerie(np.array([]), np.array([]))
         if p.ds_type == DataSeriesType.MULTICOMPONENT:
             return MultiComponentTimeSerie(
-                v.time, v.data
+                v.time.astype(np.timedelta64) / np.timedelta64(1, 's'), v.values.astype(np.float)
             ) if v else MultiComponentTimeSerie(np.array([]), np.array([]))
 
 
--- SciQLop/plugins/wind_beta.py
+++ SciQLop/plugins/wind_beta.py
@@ -1,15 +1,14 @@
 from SciQLopBindings import DataProvider, Product, ScalarTimeSerie, VectorTimeSerie, MultiComponentTimeSerie
 from SciQLopBindings import SciQLopCore, MainWindow, TimeSyncPanel, ProductsTree, DataSeriesType
 import numpy as np
-
+from datetime import datetime
 import speasy as spz
-from speasy.inventory.data_tree import amda
+from speasy.inventories import tree
 import pandas as pds
 
-b_ = amda.Parameters.Wind.MFI.wnd_mfi_kp.wnd_bmag
-n_ = amda.Parameters.Wind.SWE.wnd_swe_kp.wnd_swe_n
-vth_ = amda.Parameters.Wind.SWE.wnd_swe_kp.wnd_swe_vth
-
+b_ = tree.amda.Parameters.Wind.MFI.wnd_mfi_kp.wnd_bmag
+n_ = tree.amda.Parameters.Wind.SWE.wnd_swe_kp.wnd_swe_n
+vth_ = tree.amda.Parameters.Wind.SWE.wnd_swe_kp.wnd_swe_vth
 
 
 class WindBeta(DataProvider):
@@ -19,20 +18,23 @@ class WindBeta(DataProvider):
             [Product("/amda/Parameters/Wind/Beta", [], DataSeriesType.SCALAR, {})])
 
     def get_data(self, metadata, start, stop):
+        start = datetime.utcfromtimestamp(start)
+        stop = datetime.utcfromtimestamp(stop)
         b = spz.get_data(b_, start, stop)
         n = spz.get_data(n_, start, stop)
         vth = spz.get_data(vth_, start, stop)
         if n is None or b is None or vth is None:
             return ScalarTimeSerie(np.array([]), np.array([]))
 
-        bnt = pds.concat([b.to_dataframe(datetime_index=True), n.to_dataframe(datetime_index=True), vth.to_dataframe(datetime_index=True)])
+        bnt = pds.concat([b.to_dataframe(), n.to_dataframe(),
+                          vth.to_dataframe()])
         bnt = bnt.resample('60S').mean().interpolate()
         time = np.array([d.timestamp() for d in bnt.index])
-        b=bnt.wnd_bmag.values
-        n=bnt.wnd_swe_n.values
-        vth=bnt.wnd_swe_vth.values
-        T=vth*vth*1.67262e-27/1.380649e-23
-        beta = np.array(n*T*1.380649e-23 / (b*b*1.2566e-6*2e-18), dtype=np.float64)
+        b = bnt.wnd_bmag.values
+        n = bnt.wnd_swe_n.values
+        vth = bnt.wnd_swe_vth.values
+        T = vth * vth * 1.67262e-27 / 1.380649e-23
+        beta = np.array(n * T * 1.380649e-23 / (b * b * 1.2566e-6 * 2e-18), dtype=np.float64)
         return ScalarTimeSerie(time, beta)
 
 
