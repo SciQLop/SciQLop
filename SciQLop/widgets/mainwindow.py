3d7ebc386d29707ea8b7a53eb8191807442ed59a
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-04-13 18:09:59 +0200

    QtAds experiments
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/widgets/mainwindow.py
+++ SciQLop/widgets/mainwindow.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+import PySide6QtAds as QtAds
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtGui import QCloseEvent
 
@@ -19,11 +20,17 @@ class SciQLopMainWindow(QtWidgets.QMainWindow):
         self._setup_ui()
 
     def _setup_ui(self):
+        QtAds.CDockManager.setAutoHideConfigFlag(QtAds.CDockManager.DefaultAutoHideConfig)
+        self.dock_manager = QtAds.CDockManager(self)
+
         default_time_range = TimeRange((datetime.utcnow() - timedelta(days=361)).timestamp(),
                                        (datetime.utcnow() - timedelta(days=360)).timestamp())
-        self.setDockNestingEnabled(True)
         self.central_widget = CentralWidget(self, time_range=default_time_range)
-        self.setCentralWidget(self.central_widget)
+        central_dock_widget = QtAds.CDockWidget("Plot Area")
+        central_dock_widget.setWidget(self.central_widget)
+        central_dock_area = self.dock_manager.setCentralWidget(central_dock_widget)
+        central_dock_area.setAllowedAreas(QtAds.DockWidgetArea.OuterDockAreas)
+
         self.productTree = PyProductTree(self)
         self.add_side_pan(self.productTree)
         self.pipelinesTree = PipelineTree(self)
@@ -52,17 +59,17 @@ class SciQLopMainWindow(QtWidgets.QMainWindow):
         self.setStatusBar(self._statusbar)
 
     def add_side_pan(self, widget):
-        widget.setMinimumWidth(100)
-        self.addWidgetIntoDock(QtCore.Qt.LeftDockWidgetArea, widget,
-                               QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
-
-    def addWidgetIntoDock(self, area, widget, allowed_area=QtCore.Qt.AllDockWidgetAreas):
         if widget is not None:
-            doc = QtWidgets.QDockWidget(self)
-            doc.setAllowedAreas(allowed_area)
+            widget.setMinimumWidth(100)
+            doc = QtAds.CDockWidget(widget.windowTitle())
             doc.setWidget(widget)
-            doc.setWindowTitle(widget.windowTitle())
-            self.addDockWidget(area, doc)
+            self.dock_manager.addAutoHideDockWidget(QtAds.PySide6QtAds.ads.SideBarLocation.SideBarLeft, doc)
+
+    def addWidgetIntoDock(self, area, widget, allowed_area=QtAds.AllDockAreas):
+        if widget is not None:
+            doc = QtAds.CDockWidget(widget.windowTitle())
+            doc.setWidget(widget)
+            self.dock_manager.addDockWidget(area, doc)
 
     def new_plot_panel(self) -> TimeSyncPanel:
         return self.central_widget.new_plot_panel()
