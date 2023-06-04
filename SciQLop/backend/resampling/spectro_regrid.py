70c6d3cdd13982228146612cf0d38c7d80fc510a
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-06-04 23:13:38 +0200

    Slightly more robust spectro regrid
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/backend/resampling/spectro_regrid.py
+++ SciQLop/backend/resampling/spectro_regrid.py
@@ -6,22 +6,29 @@ from typing import Tuple
 
 def regrid(v: SpeasyVariable) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     t: np.ndarray = v.time.astype(np.timedelta64) / np.timedelta64(1, 's')
-    new_t = np.linspace(t[0], t[-1], num=min(10000, len(t)), endpoint=False)
+    resampled_t = np.linspace(t[0], t[-1], num=min(10000, len(t)), endpoint=False)
+    values = v.values
+
     if len(v.axes) == 1:
         y = np.arange(v.values.shape[1]) * 1.
-    else:
+    elif len(v.axes[1].values.shape) == 1:
         y = np.nan_to_num(v.axes[1].values.astype(np.float64), nan=0., copy=False)
-    if len(y.shape) == 2:
-        new_y = np.logspace(np.log10(np.nanmin(y)), np.log10(np.nanmax(y)), num=int(4 * y.shape[1]))
-        values = v.values
-        values = griddata((np.repeat(t, y.shape[1]), y.ravel()), values.ravel(),
-                          (np.repeat(new_t, len(new_y)), np.tile(new_y, len(new_t))), method='nearest',
-                          fill_value=np.nan)
+    elif v.axes[1].values.shape[0] == 1:
+        y = np.nan_to_num(v.axes[1].values.astype(np.float64), nan=0., copy=False)
+        y = y.flatten()
     else:
-        new_y = y
-        values = v.values
+        y = v.axes[1].values.astype(np.float64)
+        new_y = np.logspace(np.log10(np.nanmin(y)), np.log10(np.nanmax(y)), num=int(4 * y.shape[1]))
+        y = np.nan_to_num(y, nan=0., copy=False)
+
+        values = griddata((np.repeat(t, y.shape[1]), y.ravel()),
+                          values.ravel(),
+                          (np.repeat(resampled_t, len(new_y)), np.tile(new_y, len(resampled_t))),
+                          method='nearest',
+                          fill_value=np.nan)
+        y = new_y
 
     if values.dtype != np.float64:
         values = values.astype(np.float64)
 
-    return new_t, new_y, values
+    return resampled_t, y, values
