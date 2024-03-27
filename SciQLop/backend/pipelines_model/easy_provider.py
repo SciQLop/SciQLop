85e785b99780f27e47860e9da7a53330f3568f5b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-03-27 11:24:22 +0100

    Adds debug flag to get traceback from crashing user products
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/backend/pipelines_model/easy_provider.py
+++ SciQLop/backend/pipelines_model/easy_provider.py
@@ -23,7 +23,7 @@ def ensure_dt64(x_data):
 
 class EasyProvider(DataProvider):
     def __init__(self, path, callback, parameter_type: ParameterType, metadata: dict, data_order=DataOrder.Y_FIRST,
-                 cacheable=False):
+                 cacheable=False, debug=False):
         super(EasyProvider, self).__init__(name=make_simple_incr_name(callback.__name__), data_order=data_order,
                                            cacheable=cacheable)
         product_name = path.split('/')[-1]
@@ -36,10 +36,20 @@ class EasyProvider(DataProvider):
             product=Product(name=product_name, is_parameter=True, uid=product_name, provider=self.name,
                             parameter_type=parameter_type, metadata=metadata, icon="Python-logo-notext",
                             deletable=True), deletable_parent_nodes=True)
-        self._user_get_data = callback
+
+        if debug:
+            self._user_get_data = lambda start, stop: self._debug_get_data(callback, start, stop)
+        else:
+            self._user_get_data = callback
 
     def get_data(self, product, start, stop):
         return self._user_get_data(start, stop)
+
+    def _debug_get_data(self, callback, start, stop):
+        try:
+            return callback(start, stop)
+        except Exception as e:
+            print(f"Error in {self.name}: {e}")
 
     @property
     def path(self):
@@ -48,10 +58,10 @@ class EasyProvider(DataProvider):
 
 class EasyScalar(EasyProvider):
     def __init__(self, path, get_data_callback, component_name: str, metadata: dict,
-                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False):
+                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False, debug=False):
         super(EasyScalar, self).__init__(path=path, callback=get_data_callback, parameter_type=ParameterType.SCALAR,
                                          metadata={**metadata, "components": component_name}, data_order=data_order,
-                                         cacheable=cacheable)
+                                         cacheable=cacheable, debug=debug)
         self._columns = [component_name]
 
     def get_data(self, product, start, stop):
@@ -69,10 +79,10 @@ class EasyScalar(EasyProvider):
 
 class EasyVector(EasyProvider):
     def __init__(self, path, get_data_callback, components_names: List[str], metadata: dict,
-                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False):
+                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False, debug=False):
         super(EasyVector, self).__init__(path=path, callback=get_data_callback, parameter_type=ParameterType.VECTOR,
                                          metadata={**metadata, "components": ';'.join(components_names)},
-                                         data_order=data_order, cacheable=cacheable)
+                                         data_order=data_order, cacheable=cacheable, debug=debug)
         self._columns = components_names
 
     def get_data(self, product, start, stop):
@@ -90,22 +100,23 @@ class EasyVector(EasyProvider):
 
 class EasyMultiComponent(EasyVector):
     def __init__(self, path, get_data_callback, components_names: List[str], metadata: dict,
-                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False):
+                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False, debug=False):
         super(EasyVector, self).__init__(path=path, callback=get_data_callback,
                                          parameter_type=ParameterType.MULTICOMPONENT,
                                          metadata={**metadata, "components": ';'.join(components_names)},
-                                         data_order=data_order, cacheable=cacheable)
+                                         data_order=data_order, cacheable=cacheable, debug=debug)
         self._columns = components_names
 
 
 class EasySpectrogram(EasyProvider):
     def __init__(self, path, get_data_callback, metadata: dict,
-                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False):
+                 data_order: DataOrder = DataOrder.Y_FIRST, cacheable=False, debug=False):
         super(EasySpectrogram, self).__init__(path=path, callback=get_data_callback,
                                               parameter_type=ParameterType.SPECTROGRAM,
                                               metadata={**metadata},
                                               data_order=data_order,
-                                              cacheable=cacheable)
+                                              cacheable=cacheable,
+                                              debug=debug)
 
     def get_data(self, product, start, stop):
         res = self._user_get_data(start, stop)
