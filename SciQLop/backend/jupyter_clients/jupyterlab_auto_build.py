b0016fb2a792f71a880f3cb889e37051e5b163fa
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-06-10 11:18:32 +0200

    start jupyterlab anyway even if it fails to build or check if build is needed
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/backend/jupyter_clients/jupyterlab_auto_build.py
+++ SciQLop/backend/jupyter_clients/jupyterlab_auto_build.py
@@ -8,14 +8,17 @@ from SciQLop.backend.common.terminal_messages import spawn_message_dialog, close
 
 
 def main():
-    if len(build_check()) != 0:
-        widget_id = str(uuid4())
-        print(spawn_message_dialog(widget_id, "Building JupyterLab..."))
-        try:
-            build()
-        except Exception as e:
-            print(spawn_error_dialog(widget_id, str(e)))
-        print(close_message_dialog(widget_id))
+    try:
+        if len(build_check()) != 0:
+            widget_id = str(uuid4())
+            print(spawn_message_dialog(widget_id, "Building JupyterLab..."))
+            try:
+                build()
+            except Exception as e:
+                print(spawn_error_dialog(widget_id, str(e)))
+            print(close_message_dialog(widget_id))
+    except Exception as e:
+        print(str(e))
     subprocess.Popen([sys.executable] + sys.argv[1:], env=dict(os.environ), stdout=sys.stdout, stderr=sys.stderr,
                      cwd=Path.home()).wait()
 
