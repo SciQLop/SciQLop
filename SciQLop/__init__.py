f7e42f32315e4df958add95676c7d1aed772c3fd
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-08-21 14:46:21 +0200

    Bump SciQlopPlots
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/__init__.py
+++ SciQLop/__init__.py
@@ -13,7 +13,7 @@ sciqlop_root = os.path.dirname(os.path.abspath(__file__))
 
 
 def sciqlop_dependencies() -> List[str]:
-    return ['SciQLopPlots==0.8.1', 'speasy>=1.3.2', 'qtconsole', 'tscat_gui==0.4.*',
+    return ['SciQLopPlots==0.8.2', 'speasy>=1.3.2', 'qtconsole', 'tscat_gui==0.4.*',
             'tscat==0.4.*', "humanize", 'platformdirs',
             'seaborn', "scipy", "pyside6==6.7.1", "shiboken6==6.7.1", "PySide6-QtAds==4.3.0.1", "IPython",
             "ipykernel", "jupyterlab>=4,!=4.1.0",
