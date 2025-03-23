812ce9d7331bb29118bd328b929a416ee6c3c063
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-03-23 18:34:29 +0100

    Should fix zombie JupyterLab process
    Seems that SciQLop wasn't killing JupyterLab process.
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/backend/jupyter_clients/jupyterlab_auto_build.py
+++ SciQLop/backend/jupyter_clients/jupyterlab_auto_build.py
@@ -1,27 +1,24 @@
 import sys
-import os
-import subprocess
-from pathlib import Path
 from jupyterlab.commands import build, build_check
 from uuid import uuid4
 from SciQLop.backend.common.terminal_messages import spawn_message_dialog, close_message_dialog, spawn_error_dialog
-from SciQLop.backend.common.python import get_python
+from jupyterlab.labapp import main as jupyterlab_main
+
+
+def build_if_needed():
+    if 'win' not in sys.platform and len(build_check()) != 0:
+        widget_id = str(uuid4())
+        print(spawn_message_dialog(widget_id, "Building JupyterLab..."))
+        try:
+            build()
+        except Exception as e:
+            print(spawn_error_dialog(widget_id, str(e)))
+        print(close_message_dialog(widget_id))
 
 
 def main():
-    try:
-        if 'win' not in sys.platform and len(build_check()) != 0:
-            widget_id = str(uuid4())
-            print(spawn_message_dialog(widget_id, "Building JupyterLab..."))
-            try:
-                build()
-            except Exception as e:
-                print(spawn_error_dialog(widget_id, str(e)))
-            print(close_message_dialog(widget_id))
-    except Exception as e:
-        print(str(e))
-    subprocess.Popen([get_python()] + sys.argv[1:], env=dict(os.environ), stdout=sys.stdout, stderr=sys.stderr,
-                     cwd=Path.home()).wait()
+    build_if_needed()
+    jupyterlab_main(sys.argv[1:])
 
 
 if __name__ == "__main__":
