ea338e58580d8ace794885f5ba283e15bace9aab
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-12-04 15:01:09 +0100

    Add support for file download from JupyterLab
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLop/components/jupyter/ui/JupyterLabView.py
+++ SciQLop/components/jupyter/ui/JupyterLabView.py
@@ -1,5 +1,8 @@
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineDownloadRequest
 from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QFileDialog
+from pathlib import Path
 
 
 class JupyterLabView(QWebEngineView):
@@ -7,3 +10,14 @@ class JupyterLabView(QWebEngineView):
         super().__init__()
         self.setWindowTitle("SciQLop JupyterLab")
         QTimer.singleShot(100, lambda: self.setUrl(url))
+        self.page().profile().downloadRequested.connect(self.download)
+
+    def download(self, download_item: QWebEngineDownloadRequest):
+        print("Downloading", download_item)
+        path, _ = QFileDialog.getSaveFileName(self, "Save File",
+                                              f"{download_item.downloadDirectory()}/{download_item.downloadFileName()}")
+        path = Path(path).as_posix()
+        if path:
+            download_item.setDownloadDirectory(Path(path).parent.as_posix())
+            download_item.setDownloadFileName(Path(path).name)
+            download_item.accept()
