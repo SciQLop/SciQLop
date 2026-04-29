640c4dc558c053b4e4b03e6b4121774fb31cec4d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2026-04-29 23:57:56 +0200

    chore(plugins): drop experimental_collaboration
    Removes the in-tree experimental_collaboration plugin. Collaborative
    catalogs now live in the production cocat plugin under
    SciQLop/plugins/collaborative_catalogs/.

--- SciQLop/plugins/experimental_collaboration/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from SciQLop.core.ui.mainwindow import SciQLopMainWindow
-from .plugin import Plugin
-
-__all__ = ["load"]
-
-
-def load(main_window: SciQLopMainWindow):
-    return Plugin(main_window)
